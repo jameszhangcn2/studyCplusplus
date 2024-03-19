66d43daa95c5702a803e2b9350e4ae3d57144299
jianzhangee <128544511+jianzhangee@users.noreply.github.com>
2024-03-19 05:57:43 +0000

    add class

--- const/test1.cpp
+++ const/test1.cpp
@@ -1,6 +1,24 @@
 #include "stdio.h"
+
+const double ASpecRatio = 1.68;
+
+class GamePlayer {
+    static const int NumTurns = 5;
+    int scores[NumTurns];
+    public: 
+    void setScore( int score) {
+        scores[0] = score;
+    };
+    int getScore( int idx) {
+        return scores[idx];
+    };
+};
+
 int main()
 {
     printf("test \r\n");
+    GamePlayer p;
+    p.setScore(2);
+    printf("test p.scores[0] %d \r\n", p.getScore(0));
     return 0;
 }
