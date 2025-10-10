878a4ddf375b0c573103dffdae5965efbe273d79
jameszhangcn2 <128544511+jameszhangcn2@users.noreply.github.com>
2025-10-10 12:51:05 +0000

    add Singleton

---
+++ Singleton/inc/GameConfig.h
@@ -0,0 +1,15 @@
+#ifndef __GAME_CONFIG_H__
+#define __GAME_CONFIG_H__
+
+class GameConfig
+{
+    //...
+    private:
+        GameConfig(){};
+        GameConfig(const GameConfig& tmpobj);
+        GameConfig& operator = (const GameConfig& tmpobj);
+        ~GameConfig() {};
+
+};
+
+#endif
