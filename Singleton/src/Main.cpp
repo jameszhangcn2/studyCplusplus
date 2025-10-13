514b31ee56674807aa71552fe64fb6238600ef0b
jameszhangcn2 <128544511+jameszhangcn2@users.noreply.github.com>
2025-10-13 16:15:12 +0000

    use the OP

--- Singleton/src/Main.cpp
+++ Singleton/src/Main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     std::cout<<"main"<<std::endl;
     GameConfig * g_gc1 = GameConfig::getInstance();
-    g_gc1->m_test = 5;
+    g_gc1->setTestValue(5);
     std::cout<<"g_gc1->m_test "<<g_gc1->m_test<<std::endl;
     GameConfig * g_gc2 = GameConfig::getInstance();
     std::cout<<"g_gc1->m_test "<<g_gc2->m_test<<std::endl;
